64ecf546c2ac51c826ca1a9c43329dbb0ea71fd7
Oliver Sorrentino <oollii@live.fr>
2025-03-31 17:06:20 +0200

    Create Boat
    Add the boat prefab with a teleportation zone and a basic script to keep player on it

---
+++ Assets/Script/Boat.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(transform); // Set the boat as a parent of the player
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null); // Unset the boat as a parent of the player
+        }
+    }
+}
