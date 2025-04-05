983f7115b94ffe325a6fe83b5082bad05b897b32
NervzZ <julien.jordan@epfl.ch>
2025-04-05 23:35:35 +0200

    added custom teleport script to sync collider after teleporting

---
+++ Assets/Scripts/XR Rig/Locomotion/TeleportationProvider Custom.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
+
+public class CustomTeleportationProvider : TeleportationProvider
+{
+    float m_DelayTime;
+    float m_DelayStartTime;
+    protected virtual void Update()
+    {
+        if (!validRequest)
+            return;
+
+        if (locomotionState == LocomotionState.Idle)
+        {
+            if (m_DelayTime > 0f)
+            {
+                if (TryPrepareLocomotion())
+                    m_DelayStartTime = Time.time;
+            }
+            else
+            {
+                TryStartLocomotionImmediately();
+            }
+        }
+
+        if (locomotionState == LocomotionState.Moving)
+        {
+            switch (currentRequest.matchOrientation)
+            {
+                case MatchOrientation.WorldSpaceUp:
+                    upTransformation.targetUp = Vector3.up;
+                    TryQueueTransformation(upTransformation);
+                    break;
+                case MatchOrientation.TargetUp:
+                    upTransformation.targetUp = currentRequest.destinationRotation * Vector3.up;
+                    TryQueueTransformation(upTransformation);
+                    break;
+                case MatchOrientation.TargetUpAndForward:
+                    upTransformation.targetUp = currentRequest.destinationRotation * Vector3.up;
+                    TryQueueTransformation(upTransformation);
+                    forwardTransformation.targetDirection = currentRequest.destinationRotation * Vector3.forward;
+                    TryQueueTransformation(forwardTransformation);
+                    break;
+                case MatchOrientation.None:
+                    // Change nothing. Maintain current origin rotation.
+                    break;
+                default:
+                    break;
+            }
+
+            positionTransformation.targetPosition = currentRequest.destinationPosition;
+            TryQueueTransformation(positionTransformation);
+            
+            FixColliderCenter();
+
+            TryEndLocomotion();
+            validRequest = false;
+        }
+    }
+    
+    private void FixColliderCenter()
+    {
+        var xrOrigin = mediator.xrOrigin;
+        if (xrOrigin == null || xrOrigin.Camera == null || xrOrigin.Origin == null)
+            return;
+
+        var cc = xrOrigin.Origin.GetComponent<CharacterController>();
+        if (cc == null)
+            return;
+
+        var localCamPos = xrOrigin.Origin.transform.InverseTransformPoint(xrOrigin.Camera.transform.position);
+        cc.center = new Vector3(localCamPos.x, cc.center.y, localCamPos.z);
+    }
+}
